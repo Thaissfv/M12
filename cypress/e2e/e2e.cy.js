4143035237130a7197616c60fc4a7e9e72ff5563
Thais <thaissfurtado@gmail.com>
2022-11-23 16:43:21 -0300

    Só falta ajeitar o faker

--- cypress/e2e/e2e.cy.js
+++ cypress/e2e/e2e.cy.js
@@ -1,42 +1,27 @@
 /// <reference types="cypress" />
 
 const login = require('../fixtures/perfil.json')
-//import ProdutosPage from '../support/page_objects/listadeprodutos.page'
-const produtos = require('../fixtures/produtos.json')
-
+//var faker = require ('faker');
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
-    /*  Para fazer um pedido de 4 produtos 
-        Fazendo a escolha dos produtos
-        Adicionando ao carrinho
-        Preenchendo todas opções no checkout
-        E validando minha compra ao final */
 
     beforeEach(() => {
         cy.visit('/minha-conta/')
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
+        // login com usuário válido
+        var pos = 2
+        cy.login(login[pos].usuario, login[pos].senha)
 
-        //fazer login (COLOCAR AQUI UMA VARIÁVEL PARA ACESSAR O USUARIO QUE EU QUISER)
-        cy.login(login.usuario, login.senha)
-
-        // Depois de acessar, agora deve-se adicionar os produtos 4x
-       //conseguir fazer uma lista de produtos e só passar a variável de parametro
-        cy.adicionarProdutos(produtos.pagina, produtos.produto, produtos.tamanho, produtos.cor, produtos.quant)
+        // Adicionando 4 produtos diferentes
         cy.adicionarProdutos(1, 'Arcadio Gym Short', 33, 'Blue', 2)
+        cy.adicionarProdutos(3, 'Bruno Compete Hoodie', 'S', 'Blue', 2)
         cy.adicionarProdutos(37, 'Torque Power Short', 33, 'Purple', 2)
         cy.adicionarProdutos(4, 'Celeste Sports Bra', 'M', 'Green', 3)
-        //ProdutosPage.addProdutos(produtos[x].produto, produtos[x].tamanho, produtos[x].cor, produtos[x].quant)
-
 
         //fazendo checkout
-        cy.get('.dropdown-toggle > .text-skin > .icon-basket').click()
-        cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .view-cart').click()
-        cy.get('.checkout-button').click()
-        
-        // validando compra
-
+        cy.checkout('Thais', 'Vasconcelos', 'Brasil', 'Rua das Flores', 'Fortaleza', 'Rio de Janeiro', '25254-540', '54555255245', login[pos].usuario)
 
     });
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -44,3 +44,22 @@ Cypress.Commands.add('adicionarProdutos', (pagina, produto, tamanho, cor, quant)
     cy.get('.woocommerce-message').should('contain', 'foram adicionados no seu carrinho.')
 
 })
+
+Cypress.Commands.add('checkout', (nome, sobrenome, pais, rua, cidade, estado, cep, fone, email) => {
+    cy.get('.woocommerce-message > .button').click()
+    cy.get('.checkout-button').click()
+    cy.get('#billing_first_name').clear().type(nome)
+    cy.get('#billing_last_name').clear().type(sobrenome)
+    cy.get('#select2-billing_country-container').click().type(pais).get('[aria-selected="true"]').click()
+    cy.get('#billing_address_1').clear().type(rua)
+    cy.get('#billing_city').clear().type(cidade)
+    cy.get('#select2-billing_state-container').click().type(estado + '{enter}')
+    cy.get('#billing_postcode').clear().type(cep)
+    cy.get('#billing_phone').clear().type(fone)
+    cy.get('#billing_email').clear().type(email)
+    cy.get('#payment_method_cod').click()
+    cy.get('#terms').click()
+    cy.get('#place_order').click()
+    cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
+
+})
